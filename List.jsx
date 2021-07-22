79ba176a0694c2a42624411ba8573819f08db599
Guriya_kumari <81032463+guriyakumari551@users.noreply.github.com>
2021-07-22 18:44:03 +0530

    Add files via upload

---
+++ List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List()
+{
+    return
+    (<ol>
+<li>GURIYA</li>
+<li>KUMARI</li>
+<li>GUPTA</li>
+</ol>);
+   
+}
+export default List;
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Appli from './Appli.jsx';
+import './index.css';
+ReactDOM.render(
+   
+
+
+
+
+<Appli/>,
+
+
+   document.getElementById("root")
+);
